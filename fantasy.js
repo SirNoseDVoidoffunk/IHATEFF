50bed306d9b958d18577578c477c2ba656ea8101
Hansel Leal <Hansel517@Outlook.com>
2018-02-20 18:48:28 -0500

    added mobile nav functionality, added content to index

---
+++ fantasy.js
@@ -0,0 +1,11 @@
+
+//function to toggle mobile menu
+function toggleMenu(){
+	var navmenu = document.getElementById('navmenu');
+
+	if(navmenu.style.display == "flex"){
+		navmenu.style.display = "none"
+	} else {
+		navmenu.style.display = "flex"
+	}
+}
